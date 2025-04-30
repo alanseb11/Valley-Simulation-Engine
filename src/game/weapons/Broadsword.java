cc1564984cfd005ed051840b2e8306ccbfc40fa4
naomichuang <140946648+naomichuang@users.noreply.github.com>
2025-05-01 00:38:42 +1000

    Add new weapons to the game - Added Broadsword, Dragonslayer Greatsword and Katana classes

---
+++ src/game/weapons/Broadsword.java
@@ -0,0 +1,12 @@
+package game.weapons;
+
+public class Broadsword extends WeaponItem {
+
+    /**
+     * Constructor.
+     */
+    public Broadsword() {
+        super("Broadsword", 'b', 30, "slashes", 50);
+    }
+
+}
---
+++ src/game/weapons/Katana.java
@@ -0,0 +1,12 @@
+package game.weapons;
+
+public class Katana extends WeaponItem {
+
+    /**
+     * Constructor.
+     */
+    public Katana() {
+        super("Katana", 'j', 50, "slices", 60);
+    }
+
+}
