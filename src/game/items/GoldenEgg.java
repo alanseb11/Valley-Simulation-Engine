847e173f4de5a54deefee0f596eebb27a06c6313
Alan Sebastian <aseb0007@student.monash.edu>
2025-05-13 22:01:12 +1000

    Added the ability to consume the egg from the inventory

--- src/game/items/GoldenEgg.java
+++ src/game/items/GoldenEgg.java
@@ -1,11 +1,17 @@
 package game.items;
-
+import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import game.actions.EatAction;
 import game.actors.npcs.GoldenBeetle;
 import game.capabilities.Status;
+import game.interfaces.Eatable;
 
-public class GoldenEgg extends Egg{
+public class GoldenEgg extends Egg implements Eatable {
 
 
     public GoldenEgg() {
@@ -24,8 +30,6 @@ public class GoldenEgg extends Egg{
             currentLocation.removeItem(this);
             currentLocation.addActor(new GoldenBeetle());
         }
-
-
     }
 
     public boolean nearCursedTile(Location location) {
@@ -35,5 +39,21 @@ public class GoldenEgg extends Egg{
             }
         }
         return false;
+    }
+
+    @Override
+    public String eatenBy(Actor actor, GameMap map){
+        actor.modifyAttribute(BaseActorAttributes.STAMINA, ActorAttributeOperations.INCREASE,20);
+        actor.removeItemFromInventory(this);
+        return actor + " consumes the Golden Egg and restores 20 stamina!";
+    }
+
+    @Override
+    public ActionList allowableActions(Actor actor, GameMap map) {
+        ActionList actions = new ActionList();
+        if (actor.hasCapability(Status.PLAYER)) {
+            actions.add(new EatAction(this));
+        }
+        return actions;
     }
 }
