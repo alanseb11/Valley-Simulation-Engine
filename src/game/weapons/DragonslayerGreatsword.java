90db55b1a53b5c83165e39ed4846ff006ff6a4e3
owil0005 <owil0005@student.monash.edu>
2025-05-17 21:55:42 +1000

    Added javadoc comments for weapons

--- src/game/weapons/DragonslayerGreatsword.java
+++ src/game/weapons/DragonslayerGreatsword.java
@@ -6,15 +6,27 @@ import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.interfaces.Purchasable;
 
+/**
+ * Represents the Dragonslayer Greatsword.
+ * This weapon can be purchased and provides a health bonus to the buyer upon purchase.
+ */
 public class DragonslayerGreatsword extends WeaponItem implements Purchasable {
 
     /**
-     * Constructor.
+     * Constructor for the Dragonslayer Greatsword.
+     * Initializes the weapon with a name, display character, damage, verb, and hit rate.
      */
     public DragonslayerGreatsword() {
         super("Dragonslayer Greatsword", 'D', 70, "strikes", 75);
     }
 
+    /**
+     * Applies the effects of purchasing the Dragonslayer Greatsword.
+     * Increases the buyer's maximum health by 15.
+     *
+     * @param buyer the Actor who purchases the weapon
+     * @param map the GameMap where the purchase takes place
+     */
     @Override
     public void uponPurchase(Actor buyer, GameMap map) {
         buyer.modifyAttributeMaximum(BaseActorAttributes.HEALTH, ActorAttributeOperations.INCREASE, 15);
--- src/game/weapons/Katana.java
+++ src/game/weapons/Katana.java
@@ -4,15 +4,27 @@ import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.interfaces.Purchasable;
 
+/**
+ * Represents the Katana.
+ * This weapon can be purchased but has a negative effect on the buyer's health upon purchase.
+ */
 public class Katana extends WeaponItem implements Purchasable {
 
     /**
-     * Constructor.
+     * Constructor for the Katana.
+     * Initializes the weapon with a name, display character, damage, verb, and hit rate.
      */
     public Katana() {
         super("Katana", 'j', 50, "slices", 60);
     }
     
+    /**
+     * Applies the effects of purchasing the Katana.
+     * Decreases the buyer's health by 25.
+     *
+     * @param buyer the Actor who purchases the weapon
+     * @param map the GameMap where the purchase takes place
+     */
     @Override
     public void uponPurchase(Actor buyer, GameMap map) {
         buyer.hurt(25);
