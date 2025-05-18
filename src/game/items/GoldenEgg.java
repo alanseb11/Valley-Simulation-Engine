6b003705d11a7a8b29a8cc64556bacc2fb74c782
Alan Sebastian <aseb0007@student.monash.edu>
2025-05-19 00:55:24 +1000

    Added JavaDoc to the GoldenBeetle Class

--- src/game/items/GoldenEgg.java
+++ src/game/items/GoldenEgg.java
@@ -10,17 +10,48 @@ import game.capabilities.Status;
 import game.interfaces.Eatable;
 import game.monologueconditions.SurroundingCapabilityCondition;
 
+/**
+ * Represents a Golden Egg item in the game, laid by the {@link GoldenBeetle}.
+ * <p>
+ * This egg can either hatch into a new {@link GoldenBeetle} when placed near cursed ground,
+ * or be consumed by the player to restore stamina. If the egg is picked up by the player,
+ * it becomes unable to hatch.
+ * </p>
+ */
 public class GoldenEgg extends Egg implements Eatable {
+
+    /**
+     * The Golden Beetle instance that this egg will hatch into.
+     */
     private GoldenBeetle hatched = new GoldenBeetle();
 
+    /**
+     * Constructor for GoldenEgg.
+     * Inherits name, display character, and portability from {@link Egg}.
+     */
     public GoldenEgg() {
         super();
     }
 
+    /**
+     * Called every turn when the egg is in an actor's inventory.
+     * Sets {@code canHatch} to false to prevent hatching from inventory.
+     *
+     * @param currentLocation The location of the actor carrying this item.
+     * @param actor           The actor carrying the egg.
+     */
     @Override
     public void tick(Location currentLocation, Actor actor) {
         super.canHatch = false;
     }
+
+    /**
+     * Called every turn when the egg is on the ground.
+     * If the egg is near cursed ground and not already on an actor,
+     * it will hatch into a {@link GoldenBeetle}.
+     *
+     * @param currentLocation The location of the ground the egg lies on.
+     */
 
     @Override
     public void tick(Location currentLocation) {
@@ -34,6 +65,14 @@ public class GoldenEgg extends Egg implements Eatable {
         }
     }
 
+    /**
+     * Defines what happens when an actor consumes the Golden Egg.
+     * Consuming the egg restores 20 stamina and removes it from the inventory.
+     *
+     * @param actor The actor consuming the egg.
+     * @param map   The game map in which the action is occurring.
+     * @return A message indicating the result of the action.
+     */
     @Override
     public String eatenBy(Actor actor, GameMap map) {
         actor.modifyAttribute(BaseActorAttributes.STAMINA, ActorAttributeOperations.INCREASE,20);
@@ -41,6 +80,11 @@ public class GoldenEgg extends Egg implements Eatable {
         return actor + " consumes the Golden Egg and restores 20 stamina!";
     }
 
+    /**
+     * Hatches the Golden Egg into a {@link GoldenBeetle} and places it at the specified location.
+     *
+     * @param location The map location where the egg hatches.
+     */
     @Override
     public void hatch(Location location) {
         super.hatch(location);
