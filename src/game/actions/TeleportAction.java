1607512be800d8c8cf842fbfc1f432503cf4c1b4
owil0005 <owil0005@student.monash.edu>
2025-05-28 18:01:02 +1000

    Implemented teleport action, takes a destination location and teleports them to the map at the location

--- src/game/actions/TeleportAction.java
+++ src/game/actions/TeleportAction.java
@@ -1,4 +1,31 @@
 package game.actions;
 
-public class TeleportAction {
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+
+public class TeleportAction extends Action {
+    
+    private final Location destination;
+
+    public TeleportAction(Location destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        // Remove actor from current map
+        map.removeActor(actor);
+        
+        // Add actor to the destination map at the specified location
+        destination.map().addActor(actor, destination);
+        
+        return actor + " teleports to " + destination.map().toString();
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " teleports to " + destination.map().toString();
+    }
 }
