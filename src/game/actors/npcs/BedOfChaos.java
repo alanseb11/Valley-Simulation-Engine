028b8a3b3359056195760fa20fbd994d9829fc32
Alan Sebastian <aseb0007@student.monash.edu>
2025-06-03 17:30:57 +1000

    Started implemeting a BedOfChaos Class in Java for req 2

---
+++ src/game/actors/npcs/BedOfChaos.java
@@ -0,0 +1,10 @@
+import game.actors.npcs.types.NPC;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class BedOfChaos extends NPC {
+    private int hp = 1000;
+    private final int baseDamage = 25;
+    private final double accuracy = 0.75;
