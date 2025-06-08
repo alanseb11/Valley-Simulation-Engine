26d5b40d8f37ecf08fd39ac845f22e75461b1774
Alan Sebastian <aseb0007@student.monash.edu>
2025-06-08 15:52:22 +1000

    Removed unnecessary interface

--- src/game/interfaces/BossAttacker.java
+++
@@ -1,7 +0,0 @@
-package game.interfaces;
-
-import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.positions.GameMap;
-public interface BossAttacker {
-    String performAttack(Actor target, GameMap map);
-}
